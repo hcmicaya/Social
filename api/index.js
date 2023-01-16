dec76786f72fb6fbbc0c8b294df514bc4e74edf8
Hans Micaya <hcmicaya@gmail.com>
2023-01-15 21:38:26 -0500

    Setup API

---
+++ api/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import userRoutes from "./routes/users.js";
+import postsRoutes from "./routes/posts.js";
+import commentsRoutes from "./routes/comments.js";
+import authRoutes from "./routes/auth.js";
+import likesRoutes from "./routes/likes.js";
+
+const app = express();
+
+//middlewares
+app.use(express.json());
+
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postsRoutes);
+app.use("/api/comments", commentsRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/likes", likesRoutes);
+
+app.listen(8800, () => {
+    console.log("API working!");
+});
