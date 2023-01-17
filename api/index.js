9fb1c9fc5c131957dd0959689134d30561004726
Hans Micaya <hcmicaya@gmail.com>
2023-01-16 21:54:07 -0500

    Added login and logout in auth controller

--- api/index.js
+++ api/index.js
@@ -1,4 +1,6 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 import userRoutes from "./routes/users.js";
 import postsRoutes from "./routes/posts.js";
 import commentsRoutes from "./routes/comments.js";
@@ -9,6 +11,8 @@ const app = express();
 
 //middlewares
 app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postsRoutes);
