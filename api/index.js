15f8e34563833e90246a4f0e27d1ef31bd69440e
Hans Micaya <hcmicaya@gmail.com>
2023-01-19 22:33:02 -0500

    Add upload functionlity to index api

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import multer from "multer";
 import userRoutes from "./routes/users.js";
 import postsRoutes from "./routes/posts.js";
 import commentsRoutes from "./routes/comments.js";
@@ -22,6 +23,22 @@ app.use(
     })
 );
 app.use(cookieParser());
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "../client/public/upload");
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + file.originalname);
+    },
+});
+
+const upload = multer({ storage: storage });
+
+app.post("/api/upload", upload.single("file"), (req, res) => {
+    const file = req.file;
+    res.status(200).json(file.filename);
+});
 
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postsRoutes);
