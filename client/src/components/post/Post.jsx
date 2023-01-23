ae57d1bfe9a70ca8b3a6ad92785b03f2750de534
Hans Micaya <hcmicaya@gmail.com>
2023-01-22 21:37:12 -0500

    Connect comment client to database

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -62,7 +62,7 @@ const Post = ({ post }) => {
                         Share
                     </div>
                 </div>
-                {commentOpen && <Comments />}
+                {commentOpen && <Comments postId={post.id} />}
             </div>
         </div>
     );
