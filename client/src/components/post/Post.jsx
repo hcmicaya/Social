164e672ccc5a189f14e5b5c098a2124d8896be46
Hans Micaya <hcmicaya@gmail.com>
2023-01-23 22:08:11 -0500

    Added like functionality

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -8,11 +8,39 @@ import { Link } from "react-router-dom";
 import Comments from "../comments/Comments";
 import { useState } from "react";
 import moment from "moment";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { makeRequest } from "../../axios";
+import { useContext } from "react";
+import { AuthContext } from "../../context/authContextProvider";
 
 const Post = ({ post }) => {
     const [commentOpen, setCommentOpen] = useState(false);
-    //TEMPORARY
-    const liked = false;
+    const { currentUser } = useContext(AuthContext);
+    const queryClient = useQueryClient();
+
+    const { isLoading, error, data } = useQuery(["likes", post.id], () =>
+        makeRequest.get("/likes?postId=" + post.id).then((res) => {
+            return res.data;
+        })
+    );
+
+    // Mutations
+    const mutation = useMutation(
+        (liked) => {
+            if (liked) return makeRequest.delete("/likes?postId=" + post.id);
+            return makeRequest.post("/likes", { postId: post.id });
+        },
+        {
+            onSuccess: () => {
+                // Invalidate and refetch
+                queryClient.invalidateQueries(["likes"]);
+            },
+        }
+    );
+
+    const handleLike = () => {
+        mutation.mutate(data.includes(currentUser.id));
+    };
 
     return (
         <div className="post">
@@ -43,12 +71,17 @@ const Post = ({ post }) => {
                 </div>
                 <div className="info">
                     <div className="item">
-                        {liked ? (
-                            <FavoriteOutlinedIcon />
+                        {isLoading ? (
+                            "loading"
+                        ) : data.includes(currentUser.id) ? (
+                            <FavoriteOutlinedIcon
+                                style={{ color: "red" }}
+                                onClick={handleLike}
+                            />
                         ) : (
-                            <FavoriteBorderOutlinedIcon />
+                            <FavoriteBorderOutlinedIcon onClick={handleLike} />
                         )}
-                        12 Likes
+                        {data && data.length} Likes
                     </div>
                     <div
                         className="item"
